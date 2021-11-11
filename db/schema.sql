2336a6b1f9fb2191f1b8c96dbdb465eeaa9155ef
Raimeeab <raimee.abbassi@gmail.com>
2021-11-11 12:48:36 +1100

    Debug .env

--- db/schema.sql
+++ db/schema.sql
@@ -1,31 +1,38 @@
+-- Delete and recreate company database 
 DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 
+-- Apply code to company database
 USE company_db;
 
+-- Create departments table
 CREATE TABLE departments (
-    id INT PRIMARY,
+    -- Auto increment id integer, set as primary key
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    -- Set department_name variable characters to 30, must have a string the field 
     department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-    id INT PRIMARY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
+    -- Set salary to a decimal of up to 20 digits and 2 decimals 
     salary DECIMAL(20,2) NOT NULL, 
     department_id INT, 
+    -- 
     FOREIGN KEY (department_id)
             REFERENCES departments(id)
-            ON DELETE SET NULL 
+            ON DELETE CASCADE 
 );
 
 CREATE TABLE employees (
-    id INT PRIMARY, 
+    id INT AUTO_INCREMENT PRIMARY KEY, 
     first_name VARCHAR(30) NOT NULL, 
     last_name VARCHAR(30) NOT NULL,
     role_id INT, 
     FOREIGN KEY (role_id)
         REFERENCES roles(id)
-        ON DELETE SET NULL, 
+        ON DELETE CASCADE, 
     manager_id INT,
     FOREIGN KEY (manager_id) 
         REFERENCES employees(id)
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-// Import and require mysql2
+// Import and require mysql2, Inquirer & console.table
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
 
 // Set Port environment to variable PORT OR to 3001 if there's nothing there
 const PORT = process.env.PORT || 3001;
@@ -9,12 +11,16 @@ const db = mysql.createConnection(
     {
       host: 'localhost',
       // MySQL username,
-      user: 'root',
-      // TODO: UPDATE MySQL password to something else before adding 
-      password: '',
-      database: 'company_db'
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME
     },
     console.log(`Connected to the company_db database.`)
 );
 
 
+// ENTRANCE of app  / EXIST 
+
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT} 🚀`);
+//   });
